a54f3bd4443e8c78accd5038447e5d2e31bbffd2
Frank STE <stefranksu@gmail.com>
2020-09-05 16:03:45 +0800

    0.0

--- Program.cs
+++ Program.cs
@@ -1,14 +1,95 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+
+using System.Threading;
+
+using System.Net.Sockets;
+
 using System.Text;
 
-namespace ConsoleApp22
+
+
+namespace csharp_multi_threaded_server_socket
 {
+    
+    //資料來源 http://csharp.net-informations.com/communications/csharp-multi-threaded-server-socket.htm
+
     class Program
     {
         static void Main(string[] args)
         {
+            string ver = "0.0";
+            TcpListener serverSocket = new TcpListener(4001);
+            TcpClient clientSocket = default(TcpClient);
+            int counter = 0;
+            serverSocket.Start();
+            Console.WriteLine(" >> " + "Server Started");
+            counter = 0;
+            while (true)
+            {
+                counter += 1;
+                clientSocket = serverSocket.AcceptTcpClient();
+                Console.WriteLine(" >> " + "Client No:" + Convert.ToString(counter) + " started!");
+                handleClinet client = new handleClinet();
+                client.startClient(clientSocket, Convert.ToString(counter));
+            }
+            clientSocket.Close();
+            serverSocket.Stop();
+            Console.WriteLine(" >> " + "exit");
+            Console.ReadLine();
+        }
+    }
+
+
+
+    //Class to handle each client request separatly
+    public class handleClinet
+    {
+        TcpClient clientSocket;
+        string clNo;
+
+        public void startClient(TcpClient inClientSocket, string clineNo)
+        {
+            this.clientSocket = inClientSocket;
+            this.clNo = clineNo;
+            Thread ctThread = new Thread(doChat);
+            ctThread.Start();
+        }
+
+        private void doChat()
+        {
+            int requestCount = 0;
+            byte[] bytesFrom = new byte[(int)clientSocket.ReceiveBufferSize];
+            string dataFromClient = null;
+            Byte[] sendBytes = null;
+            string serverResponse = null;
+            string rCount = null;
+            requestCount = 0;
+
+            while ((true))
+            {
+                try
+                {
+                    requestCount = requestCount + 1;
+                    NetworkStream networkStream = clientSocket.GetStream();
+                    int len = networkStream.Read(bytesFrom, 0, (int)clientSocket.ReceiveBufferSize);
+                    dataFromClient = System.Text.Encoding.ASCII.GetString(bytesFrom, 0, len);
+                    //dataFromClient = dataFromClient.Substring(0, dataFromClient.IndexOf("$"));
+                    Console.WriteLine(" >> " + "From client-" + clNo + dataFromClient);
+
+                    rCount = Convert.ToString(requestCount);
+                    serverResponse = "Server to clinet(" + clNo + ") " + rCount;
+                    sendBytes = Encoding.ASCII.GetBytes(serverResponse);
+                    networkStream.Write(sendBytes, 0, sendBytes.Length);
+                    networkStream.Flush();
+                    Console.WriteLine(" >> " + serverResponse);
+                }
+                catch (Exception ex)
+                {
+                    clientSocket.Close();//jashliao修改
+                    Console.WriteLine(" >> " + "Server to clinet(" + clNo + ") closed...");// jashliao修改
+                    break; // jashliao修改
+                }
+            }
         }
     }
 }
